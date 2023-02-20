170a16359f634f0c93da9dc41daf9e68bd7568fb
Richard <kilted2000@hotmail.com>
2023-02-20 16:31:02 -0600

    test

--- app.js
+++ app.js
@@ -133,12 +133,12 @@ app.get("/about", function(req, res){
 // if(port == null || port == ""){
 //   port = 3000;
 // }
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT 
+app.listen(PORT || 3000, () => console.log(`Server running on port ${PORT}`));
 // let port = process.env.$PORT;
 
 
-// app.listen(0.0.0.0:port || 3000);
-// app.listen(port, function() {
-//   console.log("Server started successfully");
-// });
+app.listen('0.0.0.0:port' || 3000);
+app.listen(port, function() {
+  console.log("Server started successfully");
+});
